be1500b69ece68f147271e71adfd93572684faf6
João Vitor Sanches <joaovitormanolo@gmail.com>
2022-05-25 02:31:42 -0300

    Returning to work

--- src/Canvas.js
+++ src/Canvas.js
@@ -5,25 +5,34 @@ const Canvas = ({config, children = []}) => {
     const [canvas, setCanvas] = useState(null)
     const [objects, setObjects] = useState(children)
     const [settings, setConfig] = useState(config)
-    
+    let velocity = {x: 0, y: 0, mx: 0, my: 0}
+    let jumping = false
+
     const initCanvas = () => new fabric.StaticCanvas('canvas', {
         height: 800,
         width: 800,
         backgroundColor: '#d0edfc'
     })
 
-    const handleClick = (e, target, canvas) => {
+    const handleClick = (e) => {
         const keys = {
-            'ArrowDown': ['top', 1],
-            'ArrowUp': ['top', -1],
-            'ArrowLeft': ['left', -1],
-            'ArrowRight': ['left', 1]
+            'ArrowLeft': ['mx', 700],
+            'ArrowRight': ['x', 700]
         }
 
         if(Object.keys(keys).includes(e.key)) {
-            const delta = target.get(keys[e.key][0])
-            target.set(keys[e.key][0], delta + keys[e.key][1]*20)
-            canvas.renderAll()
+            velocity[keys[e.key][0]] = keys[e.key][1]
+        }
+    }
+
+    const handleStop = (e) => {
+        const keys = { 
+            'ArrowLeft': ['mx', -100],
+            'ArrowRight': ['x', 100]
+        }
+
+        if(Object.keys(keys).includes(e.key)) {
+            velocity[keys[e.key][0]] = 0
         }
     }
 
@@ -34,25 +43,29 @@ const Canvas = ({config, children = []}) => {
     }
 
     const jump = (target, canvas) => {
+        if(jumping)
+            return
         const altura = 100
-        const tempo = 1
+        const tempo = 0.5
         const tickrate = 10
         let clock = 0
-        const h0 = target.get('top')
 
-        const movement = (t) => ((2*altura*t)/tempo)*(1 - (t/tempo))
+        const vtimefunc = (t) => ((4*altura)/tempo) - ((8*altura)/(tempo**2))*(t/1000)
 
+        jumping = true
         const loop = setInterval(() => {    
-            if(clock === tempo*1000)
+            if(clock === tempo*1000) {
+                jumping = false
                 clearInterval(loop)
-            target.set("top", h0 - movement(clock/1000))
-            canvas.renderAll()
+            }
+            velocity.y = vtimefunc(clock)
             clock += tickrate
         }, tickrate)
     }
     
     useEffect(() => {
         const tempCanvas = initCanvas()
+        const tickrate = 10
         setCanvas(tempCanvas)
         tempCanvas.add(...objects)
         fabric.Image.fromURL('morius.png', function(oImg) {
@@ -62,8 +75,29 @@ const Canvas = ({config, children = []}) => {
             oImg.set('top', 800 - 0.2*height)
             const tempObjects = [...objects, oImg]
             setObjects(tempObjects)
-            window.addEventListener('keydown', (e) => handleClick(e, tempObjects[0], tempCanvas))
-            window.addEventListener('keydown', (e) => handleJump(e, tempObjects[0], tempCanvas))
+            setInterval(() => {
+                if(velocity.x === 0 && velocity.y === 0 && velocity.mx === 0 && velocity.my === 0)
+                    return
+                const x0 = tempObjects[0].get('left')
+                const y0 = tempObjects[0].get('top')
+
+                let y = y0 - (velocity.y - velocity.my)*(tickrate/1000)
+
+                if(y >= 800 - 0.2*height) {
+                    y = 800 - 0.2*height
+                    velocity.y = 0
+                }
+
+                const vx = velocity.x - velocity.mx
+                tempObjects[0].set('flipX', vx < 0)
+
+                tempObjects[0].set("left", x0 + (vx)*(tickrate/1000))
+                tempObjects[0].set("top", y)
+                tempCanvas.renderAll()
+            }, tickrate)
+            window.addEventListener('keydown', (e) => handleClick(e))
+            window.addEventListener('keyup', (e) => handleStop(e))
+            window.addEventListener('keydown', (e) => handleJump(e))
           });
     }, [])
 
